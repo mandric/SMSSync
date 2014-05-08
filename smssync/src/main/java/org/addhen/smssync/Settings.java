1ea3f1ca951e8fea911ec1a847db8951941d5569
Milan Andric <milan@medicmobile.org>
2014-05-08 14:55:15 +0300

    minor changes to sms portals settings.
    made summary more concise and variable naming more consistent.
    
    doc updates coming soon.

--- smssync/src/main/java/org/addhen/smssync/Settings.java
+++ smssync/src/main/java/org/addhen/smssync/Settings.java
@@ -69,7 +69,7 @@ public class Settings extends SherlockPreferenceActivity implements
 
     public static final String AUTO_SYNC_TIMES = "auto_sync_times";
 
-    public static final String USE_SMS_PORTALS = "use_sms_portals";
+    public static final String KEY_ENABLE_SMS_PORTALS = "enable_sms_portals";
 
     public static final String TASK_CHECK = "task_check_preference";
 
@@ -178,7 +178,7 @@ public class Settings extends SherlockPreferenceActivity implements
         autoSyncTimes.setEntries(autoSyncEntries);
         autoSyncTimes.setEntryValues(autoSyncValues);
 
-        useSmsPortals = (CheckBoxPreference) getPreferenceScreen().findPreference(USE_SMS_PORTALS);
+        useSmsPortals = (CheckBoxPreference) getPreferenceScreen().findPreference(KEY_ENABLE_SMS_PORTALS);
 
         taskCheckTimes = (ListPreference) getPreferenceScreen().findPreference(
                 TASK_CHECK_TIMES);
@@ -385,7 +385,7 @@ public class Settings extends SherlockPreferenceActivity implements
         }
 
         editor.putBoolean("UseSmsPortals", useSmsPortals.isChecked());
-        if (Prefs.useSmsPortals!=  useSmsPortals.isChecked()) {
+        if (Prefs.useSmsPortals != useSmsPortals.isChecked()) {
             boolean checked = useSmsPortals.isChecked() ? true : false;
             String check = getCheckedStatus(checked);
 
@@ -503,9 +503,9 @@ public class Settings extends SherlockPreferenceActivity implements
             }
         }
 
-        if(key.equals(USE_SMS_PORTALS)) {
+        if(key.equals(KEY_ENABLE_SMS_PORTALS)) {
             SmsPortal smsPortal = new SmsPortal(getApplicationContext());
-            if(sharedPreferences.getBoolean(USE_SMS_PORTALS, false)) {
+            if(sharedPreferences.getBoolean(KEY_ENABLE_SMS_PORTALS, false)) {
                 smsPortal.setNumber();
                 smsPortal.bindToSmsPortals();
                 availableConnections = smsPortal.getMessengers();
